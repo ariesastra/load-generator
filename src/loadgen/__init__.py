b334586677f58aa93acf071778a361900d77f157
Arie Sastra <ariesastra@Aries-MacBook-Pro.local>
2026-03-09 19:31:08 +0700

    feat(01-02): create package structure with __init__.py
    - Created src/loadgen directory structure
    - Added __init__.py with placeholder exports for future modules
    - Package can be imported successfully

--- src/loadgen/__init__.py
+++ src/loadgen/__init__.py
@@ -1,1 +1,18 @@
-"""Load generator modules."""
+"""
+Python MQTT Load Generator
+
+A load testing tool for MQTT brokers that publishes realistic
+Load Profile periodic events to benchmark data-collection services.
+"""
+
+# Export key modules for easy importing
+# Note: These modules will be implemented in subsequent plans
+# from loadgen.payload import PayloadFactory
+# from loadgen.slot_planner import SlotPlanner
+
+__all__ = [
+    # "load_meter_ids",  # Will be exported after csv_reader is implemented
+    # "MeterIdValidationError",  # Will be exported after csv_reader is implemented
+    # "PayloadFactory",  # To be implemented in plan 01-03
+    # "SlotPlanner",  # To be implemented in plan 01-04
+]
